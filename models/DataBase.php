a7e0bcf4148c8a20cb0edde355d25a4e12a3261c
JohanGM06 <desarrollo@cbit-online.com>
2024-03-28 03:15:05 -0500

    Se cambio algo del DataBase D:

--- models/DataBase.php
+++ models/DataBase.php
@@ -11,10 +11,11 @@ class DataBase
             PDO::MYSQL_ATTR_SSL_CA => 'assets/DigiCertGlobalRootCA.crt.pem'
         );
 
-        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password, $options);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
+}
     // public static function conn()
     // {
     //     $hostname = "localhost:3306";
@@ -39,4 +40,3 @@ class DataBase
     //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //     return $pdo;
     // }
-}
