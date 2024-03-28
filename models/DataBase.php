a315e9919d3d58dad0cfe33da01952f3f5698dc9
JohanGM06 <desarrollo@cbit-online.com>
2024-03-28 01:59:39 -0500

    Se cambió algo del controlador de la base de datos y se agregó un icono en la pestaña.

--- models/DataBase.php
+++ models/DataBase.php
@@ -4,9 +4,9 @@ class DataBase
     public static function conn(){
         $hostname = "localhost";
         $port = "3306";
-        $database = "id21977688_sga";
-        $username = "id21977688_root";
-        $password = "Sga2024*";
+        $database = "sga";
+        $username = "root";
+        $password = "";
 
         $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
@@ -19,6 +19,18 @@ class DataBase
     //     $database = "sga";
     //     $username = "root";
     //     $password = "";
+
+    //     $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
+    //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     return $pdo;
+    // }
+
+    // public static function conn(){
+    //     $hostname = "localhost";
+    //     $port = "3306";
+    //     $database = "id21977688_sga";
+    //     $username = "id21977688_root";
+    //     $password = "Sga2024*";
 
     //     $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
     //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- views/encabezado.php
+++ views/encabezado.php
@@ -8,6 +8,11 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="">
+    <link
+      rel="shortcut icon"
+      href="assets/img/icoon.ico"
+      type="image/x-icon"
+    />
 
     <title>SGA</title>
 
