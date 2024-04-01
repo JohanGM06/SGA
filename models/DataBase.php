f2433f2d73cc169ca926d95f6be111e03bb9760c
bryantgrippa <bryantgrippa@gmail.com>
2024-04-01 17:10:39 -0500

    volvi a meter la base de datos

--- models/DataBase.php
+++ models/DataBase.php
@@ -2,26 +2,42 @@
 class DataBase
 {
     public static function conn(){
+        $hostname = "sga.mysql.database.azure.com";
         $hostname = "localhost";
         $port = "3306";
+        $database = "sga";
+        $username = "zac";
+        $password = "SGA2024.";
+        $options = array(
+            PDO::MYSQL_ATTR_SSL_CA => 'assets/DigiCertGlobalRootCA.crt.pem'
+        );
         $database = "id21977688_sga";
         $username = "id21977688_root";
         $password = "Sga2024*";
 
+        $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password, $options);
         $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         return $pdo;
     }
+}
     // public static function conn()
     // {
     //     $hostname = "localhost:3306";
+    // public static function conn(){
+    //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    //     return $pdo;
+    // }
+
+    // public static function conn(){
+    //     $hostname = "localhost";
     //     $port = "3306";
-    //     $database = "sga";
-    //     $username = "root";
-    //     $password = "";
+    //     $database = "id21977688_sga";
+    //     $username = "id21977688_root";
+    //     $password = "Sga2024*";
 
     //     $pdo = new PDO("mysql:host=$hostname;port=$port;dbname=$database;charset=utf8", $username, $password);
     //     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     //     return $pdo;
     // }
-}
+// }
